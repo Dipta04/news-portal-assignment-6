75521014741977580cbd1966e182d918ab3b5cf9
Dipta04 <diptakarmakar38@gmail.com>
2022-09-03 15:26:22 +0600

    third

--- js/news.js
+++ js/news.js
@@ -6,7 +6,7 @@ const loadCategory=async()=>{
 }
 const displayCategory=(category)=>{
     const newaCategory=document.getElementById('news-category');
-    
+
    console.log(category);
    category.forEach(category => {
     const categoryDiv=document.createElement('div');
@@ -20,7 +20,7 @@ const displayCategory=(category)=>{
         <ul class="navbar-nav">
          
           <li class="nav-item">
-            <a onclick="loadNews('${category.category_id}')" class="nav-link" href="#">${category.category_name}</a>
+            <a onclick="loadNews('${category.category_id?category.category_id:"Nothing found"}')" class="nav-link" href="#">${category.category_name}</a>
           </li>
         </ul>
       </div>
@@ -43,22 +43,24 @@ const displayNews=shownews=>{
   
     const newsSection=document.getElementById('news-section');
     newsSection.innerHTML='';
+
    console.log(shownews);
    for(const News of shownews){
     const newsDiv=document.createElement('div');
-    newsDiv.classList.add('col');
+    // newsDiv.classList.add('col');
     newsDiv.innerHTML=`
     <div class="col">
     <div class="card p-4">
       <img src="${News.thumbnail_url}" class="card-img-top" alt="...">
       <div class="card-body">
         <h5 class="card-title">${News.title}</h5>
-        <p class="overflow">${News.details}</p>
+        <p class="overflow">${News.details?News.details.slice(0,100)+'...':News.details}</p>
         <div class="d-flex">
-        <img class="author-img rounded me-2" src="${News.author.img}">
-        <p class="card-text">${News.author.name}</p>
+        <img class="author-img rounded me-2" src="${News.author.img?News.author.img:'no image'}">
+        <p class="card-text">${News.author.name?News.author.name:"no author"}</p>
+        <i class="fa-regular fa-eye ms-4 mt-2"> ${News.total_view}</i>
         </div>
-        <button onclick="loadDetail()" class="btn btn-primary mt-2" data-bs-toggle="modal" data-bs-target="#DetailModal">Load details</button>
+        <button onclick="loadDetail('${News.category_id}')" class="btn btn-primary mt-2" data-bs-toggle="modal" data-bs-target="#DetailModal">Load details</button>
     `;
     newsSection.appendChild(newsDiv);
     
@@ -67,8 +69,8 @@ const displayNews=shownews=>{
    }
 
 
-const loadDetail=async()=>{
-    const url=`https://openapi.programming-hero.com/api/news/category/01`
+const loadDetail=async(category_id)=>{
+    const url=`https://openapi.programming-hero.com/api/news/category/${category_id}`
     const res=await fetch(url);
     const data =await res.json();
     displayDetails(data.data);
@@ -81,7 +83,7 @@ const displayDetails=(totalData)=>{
         details.innerHTML=`
         <h5 class="card-title">${fulldetail.title}</h5>
         <p>${fulldetail.details}</p>
-        <p>rating: ${fulldetail.rating.number}<p>
+        <p>rating: ${fulldetail.rating.number?fulldetail.rating.number:'not found'}<p>
         `
     }
 
